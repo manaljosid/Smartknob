9e593bd7014d181fdbaf086ce3c42d7ab63f3e78
manaljosid <mam90@hi.is>
2023-03-24 14:26:28 +0000

    Add MCP3564R register map

---
+++ Software/Smartknob/lib/MCP3564R/MCP3564R.cpp
@@ -0,0 +1,13 @@
+/*
+ *  Title: MCP3564R
+
+ *  Description: Reads data from and sends commands to the MCP3564R ADC
+ * 
+ *  Author: Mani Magnusson
+ */
+
+#include <hardware/spi.h>
+#include <hardware/gpio.h>
+#include "MCP3564R.h"
+#include "MCP3564R_regs.h"
+
--- Software/Smartknob/lib/MCP3564R/MCP3564R.h
+++ Software/Smartknob/lib/MCP3564R/MCP3564R.h
@@ -1,19 +1,45 @@
 /*
- *  Title: MT6701 Library
+ *  Title: MCP3564R
 
- *  Description: Reads data and sends commands to the MT6701 magnetic encoder
+ *  Description: Reads data from and sends commands to the MCP3564R ADC
  * 
  *  Author: Mani Magnusson
  */
 
 #pragma once
 #include <hardware/spi.h>
+#include "MCP3564R_regs.h"
+
+/* TODO:
+ - Add everything
+*/
 
 class MCP3564R {
 public:
-    MCP3564R(spi_inst_t* spi, uint csn_pin);
+    MCP3564R(spi_inst_t* spi, uint csn_pin, uint8_t addr = 0x1);
     bool init(void);
+
+    bool select_vref_source(bool internal);
+    bool set_clock_source(uint8_t source);
+    bool set_current_source_sink(uint8_t config);
+    bool set_adc_mode(uint8_t mode);
+
+    bool set_clock_prescaler(uint8_t value);
+    bool set_oversample_ratio(uint8_t ratio);
+
+    bool set_adc_bias_current(uint8_t selection);
+    bool set_adc_gain(uint8_t gain);
+    bool set_auto_zero_mux(bool enable);
+    bool set_auto_zero_ref(bool internal);
+
+    bool set_conv_mode(uint8_t mode);
+    bool set_data_format(uint8_t format);
+    bool set_crc_format(bool trailing_zeros);
+    bool set_en_crccom(bool enabled);
+    bool set_en_offcal(bool enabled);
+    bool set_en_gaincal(bool enabled);
 private:
     spi_inst_t* _spi;
     uint _csn_pin;
+    uint8_t _addr;
 };
