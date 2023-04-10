bc33f1142767698962cf3fbb53ea7f290ecccce8
manaljosid <mam90@hi.is>
2023-04-10 17:21:36 +0000

    Implement FIR library

--- Software/Smartknob/lib/FIR/FIR.cpp
+++ Software/Smartknob/lib/FIR/FIR.cpp
@@ -10,8 +10,15 @@
 #include <math.h>
 #include <stdio.h>
 #include <vector>
+#include <deque>
 #include "pico/stdlib.h"
 #include "FIR.h"
+
+template <typename T> T constrain(T amt, T low, T high) {
+    if(amt < low) return low;
+    if(amt > high) return high;
+    return amt;
+}
 
 FIR::FIR() {
     //
@@ -20,12 +27,48 @@ FIR::FIR() {
 /******************************* PUBLIC METHODS *******************************/
 
 /**
- * @brief Initialize the FOC library
- * @return True if successful, false if not
+ * @brief Initialize the FIR library. Calculates coefficients and zero-initializes the buffer.
+ * @param params Pointer to params struct containing FIR filter parameters
 */
-void FIR::init(void) {
-    //gpio_init(_csn_pin);
-    //gpio_set_dir(_csn_pin, GPIO_OUT);
-    //gpio_pull_up(_csn_pin);
-    //gpio_put(_csn_pin, true);
+void FIR::init(FIR_params* params) {
+    _params = *params;
+    _coeffs = calculate_coeffs(params);
+    for(uint i = 0; i < params->length; i++) {
+        _buffer.push_front(0.0f);
+    }
+}
+
+float FIR::run(float input) {
+    _buffer.push_front(input);
+    _buffer.pop_back();
+    float output = 0.0f;
+    for(uint i = 0; i < _params.length; i++) {
+        output += _coeffs[i] * _buffer[i];
+    }
+    return output;
+}
+
+/******************************* PRIVATE METHODS *******************************/
+
+/**
+ * @brief Calculate coefficients of a FIR filter. Uses a Hamming window and type 1 FIR filter (symmetric sequence of odd length)
+ * @param params Pointer to params struct containing FIR filter parameters
+ * @return std::vector of type float containing the coefficients
+*/
+std::vector<float> FIR::calculate_coeffs(FIR_params* params) {
+    float discrete_frequency = params->cutoff * 2.0f * _pi / params->sample_rate; // rad/s
+    if(params->type == Type::HPF) discrete_frequency = _pi - discrete_frequency;
+    uint length = params->length;
+    if(length % 2 == 0) length++; // If length is even number make it odd by adding 1
+    std::vector<float> coeffs;
+    for(uint n = -((length-1)/2); n <= (length-1)/2; n++) {
+        if(n == 0) {
+            coeffs.push_back(1.0f);
+        } else if(params->type == Type::HPF) {
+            coeffs.push_back(pow(-1.0f, float(n))*(sin(discrete_frequency * float(n)) / _pi * float(n)) * (0.54f * 0.46f * cos(2.0f * _pi * n / float(length))));
+        } else {
+            coeffs.push_back((sin(discrete_frequency * float(n)) / _pi * float(n)) * (0.54f * 0.46f * cos(2.0f * _pi * n / float(length))));
+        }
+    }
+    return coeffs;
 }
--- Software/Smartknob/lib/FIR/FIR.h
+++ Software/Smartknob/lib/FIR/FIR.h
@@ -8,6 +8,7 @@
 
 #pragma once
 #include <vector>
+#include <deque>
 /* TODO:
  - Add everything
 */
@@ -15,8 +16,36 @@
 class FIR {
 public:
     FIR();
-    void init(void);
 
-    bool update(void);
+    /**
+     * @brief FIR filter type
+    */
+    enum Type : uint8_t {
+        LPF = 0,
+        HPF = 1
+    };
+
+    /**
+     * @brief Struct containing the parameters for a FIR filter instance
+     * @param type FIR filter type LPF or HPF (see FIR::Type)
+     * @param length Length of the filter
+     * @param cutoff Filter cutoff frequency in Hz
+     * @param sample_rate Filter sample rate in Hz
+    */
+    struct FIR_params {
+        Type type;
+        uint length;
+        float cutoff;
+        float sample_rate;
+    };
+    
+    void init(FIR_params* params);
+    float run(float input);
 private:
+    const float _pi = 3.14159265358979323846f;
+    uint index = 0;
+    FIR_params _params;
+    std::vector<float> _coeffs;
+    std::deque<float> _buffer;
+    std::vector<float> calculate_coeffs(FIR_params* params);
 };
